7df19fabf26cac23a6761fd90bdc415419910377
aleksq7 <morrisalex2001@gmail.com>
2021-08-10 11:49:16 -0600

    commit tree 73c541d48d1e9efb7d025f0bb8f0b0af660004d4

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Your code here
+function razzle(lawyer='Billy', target="'em") {
+    console.log(`${lawyer} razzle-dazzles ${target}`);
+}
+
+function saturdayFun(activity='roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function(activity='go to the office') {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(symbol='*'){
+    return function(adj='special'){
+        return `You are ${symbol}${adj}${symbol}!`
+    }
+}
